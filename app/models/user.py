c7e983b42fe139ad57d6f8aeb171c940a6cd9bb0
Parsa Zeinali <pzeinali4@gmail.com>
2024-08-16 18:46:32 +0330

    chore: Add GitHub ID field to User model

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,9 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    github_id = db.Column(db.Integer, unique=True, nullable=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=True)
     name = db.Column(db.String(100), nullable=False)
     picture = db.Column(db.String(100))
     session_token = db.Column(db.String(500), nullable=False)
