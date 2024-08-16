828e15bebbb9b286d4cf55d04ca26af0b7e334f3
Parsa Zeinali <pzeinali4@gmail.com>
2024-08-16 20:37:10 +0330

    chore: Update auth.py with JWT token decoding and user retrieval

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,53 +1,36 @@
+from datetime import datetime
 from flask import jsonify, current_app, request
 from functools import wraps
 from flask_dance.contrib.google import make_google_blueprint
 import requests
+import jwt
+from uuid import UUID
+from app.models.user import User
 
-# def setup_oauth(app):
-#     oauth_bp = make_google_blueprint(
-#         client_id=app.config['OAUTH_CLIENT_ID'],
-#         client_secret=app.config['OAUTH_CLIENT_SECRET'],
-#         scope=["profile", "email"],
-#         redirect_to="main.callback"  # Ensure you have a callback route defined
-#     )
-#     app.register_blueprint(oauth_bp, url_prefix="/login")
-#     return oauth_bp
+import jwt
+from typing import TypedDict, Optional
 
-# def oauth_required(f):
-#     @wraps(f)
-#     def decorated(*args, **kwargs):
-#         auth_header = request.headers.get('Authorization')
-#         current_app.logger.info(f"Authorization header: {auth_header}")
-#         if not auth_header:
-#             current_app.logger.error("No Authorization header present")
-#             return jsonify({"msg": "Missing Authorization header"}), 401
+class DecodedToken(TypedDict):
+    username: str
+    email: str
+    name: str
+    picture: str
+    exp: int
 
-#         parts = auth_header.split()
-#         if parts[0].lower() != 'bearer':
-#             current_app.logger.error("Authorization header must start with Bearer")
-#             return jsonify({"msg": "Invalid Authorization header"}), 401
-#         elif len(parts) == 1:
-#             current_app.logger.error("Token not found in Authorization header")
-#             return jsonify({"msg": "Token not found"}), 401
-#         elif len(parts) > 2:
-#             current_app.logger.error("Authorization header must be Bearer token")
-#             return jsonify({"msg": "Invalid Authorization header"}), 401
-
-#         token = parts[1]
-#         try:
-#             headers = {'Authorization': f'Bearer {token}'}
-#             resp = requests.get('https://www.googleapis.com/oauth2/v3/userinfo', headers=headers)
-#             if resp.status_code == 200:
-#                 # Token is valid
-#                 return f(*args, **kwargs)
-#             else:
-#                 current_app.logger.error(f"OAuth2 token validation failed: {resp.text}")
-#                 return jsonify({"msg": "Invalid token"}), 401
-#         except Exception as e:
-#             current_app.logger.error(f"OAuth2 token validation failed: {str(e)}")
-#             return jsonify({"msg": f"{str(e)}"}), 401
-
-#     return decorated
+def decode_token(stored_token: str) -> Optional[DecodedToken]:
+    try:
+        decoded = jwt.decode(stored_token, options={"verify_signature": False})
+        return DecodedToken(
+            id=decoded.get('id', ''),
+            username=decoded.get('username', ''),
+            email=decoded.get('email', ''),
+            name=decoded.get('name', ''),
+            picture=decoded.get('picture', ''),
+            exp=decoded.get('exp', 0)
+        )
+    except jwt.DecodeError:
+        print("Failed to decode token")
+        return None
 
 def oauth_required(f):
     @wraps(f)
@@ -74,15 +57,16 @@ def oauth_required(f):
         
         try:
             # Decode the session token
-            decoded_token = decode_session_token(session_token)
+            decoded_token = decode_token(session_token)
             
             # Check if the token has expired
-            if decoded_token['exp'] < datetime.utcnow().timestamp():
+            if decoded_token['exp'] < datetime.now().timestamp():
                 current_app.logger.error("Session token has expired")
                 return jsonify({"msg": "Session expired"}), 401
             
             # Get the user from the database using the session token
-            user = get_user_by_session_token(session_token)
+            
+            user = User.query.filter_by(id=UUID(decoded_token['id'])).first()
             
             if not user:
                 current_app.logger.error("No user found for the given session token")
