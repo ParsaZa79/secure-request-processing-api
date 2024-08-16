f7c099fe0cbc3f75a1fe76f19fb69d361a803af5
Parsa Zeinali <pzeinali4@gmail.com>
2024-08-16 18:46:23 +0330

    chore: Update config.py with GitHub OAuth client ID and secret

--- config.py
+++ config.py
@@ -6,12 +6,15 @@ class Config:
     # Base Configurations
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
-    JWT_SECRET_KEY=os.getenv('JWT_SECRET')
+    JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY')
     API_URL=os.getenv('API_URL')
     
     # OAuth Configurations
-    OAUTH_CLIENT_ID=os.getenv('OAUTH_CLIENT_ID')
-    OAUTH_CLIENT_SECRET=os.getenv('OAUTH_CLIENT_SECRET')
+    GOOGLE_OAUTH_CLIENT_ID=os.getenv('GOOGLE_OAUTH_CLIENT_ID')
+    GOOGLE_OAUTH_CLIENT_SECRET=os.getenv('GOOGLE_OAUTH_CLIENT_SECRET')
+    
+    GITHUB_OAUTH_CLIENT_ID=os.getenv('GITHUB_OAUTH_CLIENT_ID')
+    GITHUB_OAUTH_CLIENT_SECRET=os.getenv('GITHUB_OAUTH_CLIENT_SECRET')
     
     # SESSION-Related Configurations
     SESSION_TYPE='filesystem'
