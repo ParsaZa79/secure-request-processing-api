f76ec363a22e444408f7b80860ddf37c6983e760
Parsa Zeinali <pzeinali4@gmail.com>
2024-08-16 20:36:56 +0330

    chore: Update User model with UUID primary key and Google ID field

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,16 @@
 from app import db
+import uuid
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     github_id = db.Column(db.Integer, unique=True, nullable=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
+    google_id = db.Column(db.String(50), unique=True, nullable=True)
+    username = db.Column(db.String(100), unique=True, nullable=True)
     email = db.Column(db.String(100), unique=True, nullable=True)
     name = db.Column(db.String(100), nullable=False)
     picture = db.Column(db.String(100))
-    session_token = db.Column(db.String(500), nullable=False)
+    session_token = db.Column(db.String(500), nullable=True)
     session_expiration = db.Column(db.DateTime(timezone=True), nullable=False)
     
     def __repr__(self):
